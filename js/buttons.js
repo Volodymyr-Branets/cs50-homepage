0c7f303cff35baffc8cb3eb2c7e85cbede544256
Volodymyr <branets.volodymyr@gmail.com>
2022-07-17 14:07:26 +0300

    Change color on logo in all pages, add link on BuySmartphone button

---
+++ js/buttons.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let btnBuy = document.querySelector('.btn-by-phone');
+    btnBuy.addEventListener('click', function() {
+        btnBuy.style.bacgroundColor = 'green';
+    });
+});
